bc6c495564e7470b8634983999fcd680343c3721
Skarn <skarn@ti-ssi.com>
2020-04-08 22:38:00 +0000

    fix absolute path

--- game/screens/play.vue.js
+++ game/screens/play.vue.js
@@ -5,7 +5,7 @@ import commandsView from '../views/commandsView.vue.js';
 
 export default
 function(resolve){
-    $.getJSON( '/game/maps/2.json', function( json ) {
+    $.getJSON( './game/maps/2.json', function( json ) {
     	Game.state.map = json;
 
     resolve({
