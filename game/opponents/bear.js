b6bde877525e34eb40e11107a21662368ddedab4
1341233 <1341233@cll.qc.ca>
2020-04-14 14:13:41 -0400

    attribution de l'id #2 à l'ours

--- game/opponents/bear.js
+++ game/opponents/bear.js
@@ -3,7 +3,6 @@ import {Game} from '../lib/game.js';
 
 export default class extends Opponent{
     constructor(waveNb){
-	super(waveNb*10);
-	this.speed = 1 * Game.state.map.meta[Game.state.map.path[0]].h / 100;
+	super(waveNb*10,1,2);
     }
 }
